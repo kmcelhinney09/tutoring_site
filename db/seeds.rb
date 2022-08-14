7f0b2f07091aa0cdde7bda6988c5dc38ead36885
Kevin <mr.mcelhinney@gmail.com>
2022-08-13 20:33:05 -0700

    start sign_up back end

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+# 10.times do
+#   fake_name = Faker::Name.name
+#   fake_email = Faker::internet.email
+#   fake_school = Faker::University.name
+#   grade = rand (1..16)
+#   password = '123Abc'
+# end
