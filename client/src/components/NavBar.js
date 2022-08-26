d3c7744a387c00ee8e5195c37bd5576ed8258b8b
Kevin <mr.mcelhinney@gmail.com>
2022-08-25 20:11:52 -0700

    Initiated NavBar component

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+function NavBar() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark"></Navbar>
+  );
+}
+
+export default NavBar;
