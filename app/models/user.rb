1bacfda36f57d3a9b555aa81a2c72238d27fe65c
Kevin <mr.mcelhinney@gmail.com>
2022-08-25 20:58:37 -0700

    Added enum to user roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
+  enum role: [:tutee, :tutor, :teacher, :admin]
+  after_initialize do
+    if self.new_record?
+      self.role ||= :tutee
+    end
+  end
+
   #password validation was adapted from chowderhead on dev.to at https://dev.to/nodefiend/rails-password-validation-29kj
   validate :password_lower_case
   validate :password_uppercase
