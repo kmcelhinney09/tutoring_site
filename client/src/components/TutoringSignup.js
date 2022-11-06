e517cc5ec25c9f3c993f1de8da536aeaacb2c622
Kevin <mr.mcelhinney@gmail.com>
2022-11-05 21:51:53 -0700

    removed console log

--- client/src/components/TutoringSignup.js
+++ client/src/components/TutoringSignup.js
@@ -1,18 +1,15 @@
 import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
-import Col from "react-bootstrap/esm/Col";
 import Table from "react-bootstrap/Table";
 
 function TutoringSignup({ school_id }) {
-  console.log(school_id);
   const [tutoringInfo, setTutoringInfo] = useState(false);
 
   useEffect(() => {
     fetch(`/school/${school_id}/tutoring`).then((res) => {
       if (res.ok) {
         res.json().then((data) => {
-          console.log(data);
           setTutoringInfo(data);
         });
       }
@@ -23,18 +20,18 @@ function TutoringSignup({ school_id }) {
     let slot_status;
 
     if (slot_info.booked_status === false) {
-      slot_status = 'Sign-up'
+      slot_status = "Sign-up";
     } else {
-      slot_status = 'Full'
+      slot_status = "Full";
     }
     return (
       <tr>
-          <td>{slot_info.date}</td>
-          <td>{slot_info.start_time}</td>
-          <td>{slot_info.end_time}</td>
-          <td>{slot_status}</td>
-        </tr>
-    )
+        <td>{slot_info.date}</td>
+        <td>{slot_info.start_time}</td>
+        <td>{slot_info.end_time}</td>
+        <td>{slot_status}</td>
+      </tr>
+    );
   }
   return (
     <>
@@ -44,41 +41,39 @@ function TutoringSignup({ school_id }) {
             <h1>{tutoringInfo.school_name}</h1>
           </Row>
           <Row>
-            <Row>
-              {tutoringInfo.locations.map((location) => {
-                return (
-                  <Row key={location.id}>
-                    <h3>{location.building}</h3>
-                    <Row>
-                      <h6>{location.name}</h6>
-                    </Row>
-                    <Row>
-                      <Table>
-                        <thead>
-                          <tr>
-                            <th>Session</th>
-                            <th>Start Time</th>
-                            <th>End Time</th>
-                            <th>Status</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {tutoringInfo.tutoring_time_slots.map((slot) => {
-                            return (
-                              <>
-                                {slot.location_id == location.id ? (
-                                  tutoring_slots(slot)
-                                ) : null}
-                              </>
-                            );
-                          })}
-                        </tbody>
-                      </Table>
-                    </Row>
+            {tutoringInfo.locations.map((location) => {
+              return (
+                <Row key={location.id}>
+                  <h3>{location.building}</h3>
+                  <Row>
+                    <h6>{location.name}</h6>
                   </Row>
-                );
-              })}
-            </Row>
+                  <Row>
+                    <Table>
+                      <thead>
+                        <tr>
+                          <th>Session</th>
+                          <th>Start Time</th>
+                          <th>End Time</th>
+                          <th>Status</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {tutoringInfo.tutoring_time_slots.map((slot) => {
+                          return (
+                            <>
+                              {slot.location_id == location.id
+                                ? tutoring_slots(slot)
+                                : null}
+                            </>
+                          );
+                        })}
+                      </tbody>
+                    </Table>
+                  </Row>
+                </Row>
+              );
+            })}
           </Row>
         </Container>
       ) : (
